fde983d5147923f7ca6813207289f3a9c9100ea6
Angel <angelhodar76@gmail.com>
2021-02-01 23:33:48 +0100

    Added initial test

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,47 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+export default NextAuth({
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  database: process.env.DATABASE_URL_LOCAL,
+  secret: process.env.AUTH_SECRET,
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    jwt: true,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+  callbacks: {
+    /**
+     * @param  {object} session      Session object
+     * @param  {object} token        User object    (if using database sessions)
+     *                               JSON Web Token (if not using database sessions)
+     * @return {object}              Session that will be returned to the client
+     */
+    async session(session, token) {
+      if (token?.labelityToken) {
+        // Add property to session, like an access_token from a provider
+        session.labelityToken = token.labelityToken;
+      }
+      return session;
+    },
+    /**
+     * @param  {object}  token     Decrypted JSON Web Token
+     * @param  {object}  user      User object      (only available on sign in)
+     * @param  {object}  account   Provider account (only available on sign in)
+     * @param  {object}  profile   Provider profile (only available on sign in)
+     * @param  {boolean} isNewUser True if new user (only available on sign in)
+     * @return {object}            JSON Web Token that will be saved
+     */
+    async jwt(token, user, account, profile, isNewUser) {
+      token.labelityToken = "test18hf7sfdyisddfby";
+      return token;
+    },
+  },
+});
