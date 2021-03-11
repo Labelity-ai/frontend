038ce6400936b6d7440e0d4c0952925c943edf29
Antonio Molner <antoniomdk@hotmail.es>
2021-03-11 19:27:00 +0100

    Add initial version of annotations file import modal

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  Flex, Box, Tabs, TabList, TabPanels, Tab, TabPanel, HStack
+  Box, Tabs, TabList, TabPanels, Tab, TabPanel, HStack, Button,
 } from '@chakra-ui/react';
 import { useSession } from 'next-auth/client';
 import { useRouter } from 'next/router';
 
+import { FaFileImport } from 'react-icons/fa';
 import SideNavigation from '../components/SideNavigation';
 import DashboardContainer from '../components/DashboardContainer';
 import QueryBuilderTab from '../components/QueryBuilderTab';
+import ImportAnnotationsModal from '../components/ImportAnnotationsModal';
 
 const Dashboard = () => {
   const router = useRouter();
   const [session, loading] = useSession();
+  const [importModalOpen, setImportModalOpen] = useState(false);
 
   React.useEffect(() => {
     // if (!loading && !session) router.push('/login');
@@ -21,6 +24,7 @@ const Dashboard = () => {
 
   return (
     <HStack height="100vh">
+      <ImportAnnotationsModal open={importModalOpen} onClose={() => setImportModalOpen(false)} />
       <Box height="100vh" bgColor="gray.900">
         <SideNavigation />
       </Box>
@@ -28,6 +32,17 @@ const Dashboard = () => {
         <TabList paddingLeft="20px">
           <Tab>Image View</Tab>
           <Tab>Query Builder</Tab>
+          <Button
+            marginTop="auto"
+            marginBottom="auto"
+            size="sm"
+            marginLeft="auto"
+            marginRight="20px"
+            onClick={() => setImportModalOpen(true)}
+            leftIcon={<FaFileImport />}
+          >
+            Import Annotations
+          </Button>
         </TabList>
         <TabPanels height="95vh">
           <TabPanel height="100%">
